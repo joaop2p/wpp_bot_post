0448b28d7b4224fc9b2d279150ddfb9b9a742f54
João Paulo <62178699+joaop2p@users.noreply.github.com>
2025-08-19 09:23:22 -0300

    Delete src directory

--- src/utils/file_selector.py
+++
@@ -1,8 +0,0 @@
-from tkinter.filedialog import askopenfilename
-
-
-def file_selector() -> str:
-    file = askopenfilename(filetypes=[("Text with headers", "*txt")])
-    if len(file) < 1:
-        raise Exception("Nenhum arquivo foi selecionado")
-    return file
